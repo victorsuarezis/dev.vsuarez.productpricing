36e98d86761872dbb628604a666ae456a596fcc2
Ing. Victor Suarez <victor.suarez.is@gmail.com>
2023-06-28 17:05:34 -0400

    Added discount for Brand Attribute

--- src/dev/vsuarez/model/IVS_MDiscountSchemaBreak.java
+++ src/dev/vsuarez/model/IVS_MDiscountSchemaBreak.java
@@ -9,6 +9,7 @@ import java.util.Properties;
 
 import org.compiere.model.MDiscountSchemaBreak;
 import org.compiere.model.MProductCategory;
+import org.compiere.util.DB;
 
 /**
  * @author <a href="mailto:victor.suarez.is@gmail.com">Ing. Victor Suarez</a>
@@ -23,6 +24,7 @@ public class IVS_MDiscountSchemaBreak extends MDiscountSchemaBreak {
 	
 	private static final String COLUMNNAME_M_Warehouse_ID = "M_Warehouse_ID";
 	private static final String COLUMNNAME_M_PriceList_ID = "M_PriceList_ID";
+	private static final String COLUMNNAME_IVS_Brand_ID = "IVS_Brand_ID";
 
 	/**
 	 * @param ctx
@@ -88,9 +90,9 @@ public class IVS_MDiscountSchemaBreak extends MDiscountSchemaBreak {
 		if (Value.compareTo(getBreakValue()) < 0)
 			return false;
 		
-		//	No Product / Category / Warehouse / PriceList
+		//	No Product / Category / Warehouse / PriceList / Brand
 		if (getM_Product_ID() == 0 && getM_Product_Category_ID() == 0
-				&& getM_Warehouse_ID() == 0 && getM_PriceList_ID() == 0)
+				&& getM_Warehouse_ID() == 0 && getM_PriceList_ID() == 0 && getIVS_Brand_ID() == 0)
 			return true;
 		
 		//	Product
@@ -100,12 +102,19 @@ public class IVS_MDiscountSchemaBreak extends MDiscountSchemaBreak {
 		// Warehouse and Price List
 		if(getM_Warehouse_ID() > 0 && getM_PriceList_ID() > 0)
 			return getM_Warehouse_ID() == M_Warehouse_ID && getM_PriceList_ID() == M_PriceList_ID;
+		
+		// Brand and Price List
+		if(getIVS_Brand_ID() > 0 && getM_PriceList_ID() > 0)
+			return getIVS_Brand_ID() == getIVS_Brand_ID(M_Product_ID) && getM_PriceList_ID() == M_PriceList_ID; 
+		
 		if(getM_Warehouse_ID() > 0)
 			if(getM_Warehouse_ID() == M_Warehouse_ID)
 				return true;
 		if(getM_PriceList_ID() > 0)
 			if(getM_PriceList_ID() == M_PriceList_ID)
 				return true;
+		if(getIVS_Brand_ID() > 0)
+			return getIVS_Brand_ID() == getIVS_Brand_ID(M_Product_ID);
 		
 		//	Category
 		if (M_Product_Category_ID != 0)
@@ -114,10 +123,10 @@ public class IVS_MDiscountSchemaBreak extends MDiscountSchemaBreak {
 		//	Look up Category of Product
 		return MProductCategory.isCategory(getM_Product_Category_ID(), M_Product_ID);
 	}	//	applies
-	
+
 	/**
 	 * Get Warehouse 
-	 * @return
+	 * @return M_Warehouse_ID
 	 */
 	public int getM_Warehouse_ID() {
 		return get_ValueAsInt(COLUMNNAME_M_Warehouse_ID );
@@ -125,7 +134,7 @@ public class IVS_MDiscountSchemaBreak extends MDiscountSchemaBreak {
 	
 	/**
 	 * Get Price List
-	 * @return
+	 * @return M_PriceList_ID
 	 */
 	public int getM_PriceList_ID() {
 		return get_ValueAsInt(COLUMNNAME_M_PriceList_ID);
@@ -146,5 +155,33 @@ public class IVS_MDiscountSchemaBreak extends MDiscountSchemaBreak {
 		if(M_PriceList_ID > 0)
 			set_Value(COLUMNNAME_M_PriceList_ID, M_PriceList_ID);
 	}
-
+	
+	/**
+	 * Set Brand
+	 */
+	public void setIVS_Brand_ID(int IVS_Brand_ID) {
+		if(IVS_Brand_ID > 0)
+			set_Value(COLUMNNAME_IVS_Brand_ID, IVS_Brand_ID);
+	}
+	
+	/**
+	 * Get Brand
+	 * @return IVS_Brand_ID
+	 */
+	public int getIVS_Brand_ID() {
+		return get_ValueAsInt(COLUMNNAME_IVS_Brand_ID);
+	}
+	
+	/**
+	 * Brand from Product
+	 * @param M_Product_ID
+	 * @return IVS_Brand_ID
+	 */
+	public int getIVS_Brand_ID(int M_Product_ID) {
+		String sql = "SELECT ai.M_AttributeValue_ID FROM M_AttributeInstance ai "
+				+ " JOIN M_Product p ON p.M_AttributeSetInstance_ID = ai.M_AttributeSetInstance_ID "
+				+ " WHERE p.M_Product_ID = ? AND ai.M_AttributeValue_ID =?";
+		return DB.getSQLValue(get_TrxName(), sql, M_Product_ID, getIVS_Brand_ID());
+	}
+	
 }
